5cb71e1799736d8be02bf3917271da025a8d695a
Rafael_O <ralph.orozco@gmail.com>
2020-12-19 13:47:24 -0800

    adding the models, schema, and env

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-var message = "HELLO";
-console.log(message);
+//server
+const db = require("./models");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+db.sequelize.sync().then(() => {
+  //spin up express server in here after db is connected
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("working!");
+  });
+});
