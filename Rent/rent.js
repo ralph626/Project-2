2c78a40e4a4603b037e4b689c2eb116311614d9f
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-19 10:55:21 -0800

    ADDED A JS FILE FOR THE RENT API

---
+++ Rent/rent.js
@@ -0,0 +1,4 @@
+var apiKey = "b7c412f502mshbbc78eb664a4ee7p1c8f5fjsn8b018315a9c2";
+var rentURL = "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice";
+
+//=============RETRIEVING THE ARRAY OF INFO FROM THE RENT API===========//
