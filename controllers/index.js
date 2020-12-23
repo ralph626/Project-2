425c22b1075db501f6c984e304faf8f504058719
NPitroff <40879599+NPitroff@users.noreply.github.com>
2020-12-22 21:48:31 -0800

    Update the routes and synch branch

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
 
+// const apiRoutes = require('./api');
+const homeRoutes = require('./home-routes');
+//  const dashboardRoutes = require('.rentSearch-routes');
+
+// the following paths
 router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+// router.use('/rentSearch', dashboardRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
