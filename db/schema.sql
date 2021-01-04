5587d77f1913c83b22f0ba535f7459a28bc4fa99
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-04 07:25:04 -0800

    Added commands in the schema to create a user table

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,13 @@
+-- ======DROP DATABASE=================
 DROP DATABASE IF EXISTS goHome_db;
+-- =======CREATE THE DATABASE============
 CREATE DATABASE goHome_db;
+-- ==============USE THE DATABASE==========
+USE goHome_db;
+-- =============CREATE A TABLE FOR USER INFORMATION=======
+CREATE TABLE user(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    pass_word VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
