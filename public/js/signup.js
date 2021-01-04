9417e1181e312a361cc8080ab9400a89b52be9b5
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-04 11:30:28 -0800

    Created a new folder for the html files, and created a verification for the email address

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,6 +1,11 @@
+// const { rules } = require("eslint-config-prettier");
+const key = "b7c412f502mshbbc78eb664a4ee7p1c8f5fjsn8b018315a9c2";
+const url = "mailcheck.p.rapidapi.com";
+let email = document.querySelector("#signup-email").value.trim();
 
 
-fetch("https://mailcheck.p.rapidapi.com/?domain=mailinator.com", {
+function newEmailVerification(){
+fetch("https://mailcheck.p.rapidapi.com/?domain="+ `${email}`, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "b7c412f502mshbbc78eb664a4ee7p1c8f5fjsn8b018315a9c2",
@@ -13,3 +18,4 @@ fetch("https://mailcheck.p.rapidapi.com/?domain=mailinator.com", {
 .catch(err => {
 	console.error(err);
 });
+}
