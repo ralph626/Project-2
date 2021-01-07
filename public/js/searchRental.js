a45bda975591aa0dab22d00e75d4a002d0caa375
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-07 10:46:09 -0800

    Fixed AJAX route and now it can display data

--- public/js/searchRental.js
+++ public/js/searchRental.js
@@ -42,7 +42,7 @@ $(document).ready(function () {
     const settings = {
         "async": true,
         "crossDomain": true,
-        "url": rentalURL +  compcount + "&" + squareFootage + "&" + bathrooms + "&" + "&" + bedrooms + "&" + propertyType,
+        "url": rentalURL +  "compCount="+compCount + "&" + "squareFootage="+squareFootage + "&" + "bathrooms="+bathrooms + "&" + "address="+address + "&" + "bedrooms="+bedrooms + "&" + "propertyType="+propertyType,
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "b7c412f502mshbbc78eb664a4ee7p1c8f5fjsn8b018315a9c2",
@@ -51,9 +51,9 @@ $(document).ready(function () {
     };
     
     $.ajax(settings).done(function (response) {
-        console.log(response);
+        console.log("The AJAX was successfull",response);
         //   function to fill out listing data===================
-        listingData(result);
+        listingData(response);
     });
 
     //axios call======================================
@@ -81,18 +81,18 @@ $(document).ready(function () {
   });
 });
 //calling function for listing data=================
-listingData = (response) => {
-  console.log(response);
+function listingData(result){
+  console.log(result);
   //variables for the function call from the json of the rental api
-  let listRent = response.rent;
-  let rentRangeLow = response.rentRangeLow;
-  let rentRangeHigh = response.rentRangeHigh;
-  let address = response.listings[0].formattedAddress;
-  let price = response.listings[0].price;
-  let bedrooms = response.listings[0].bedrooms;
-  let bathrooms = response.listings[0].bathrooms;
-  let propType = response.listings[0].propertyType;
-  let sqrfootage = response.listings[0].squareFootage;
+  let listRent = result.rent;
+  let rentRangeLow = result.rentRangeLow;
+  let rentRangeHigh = result.rentRangeHigh;
+  let address = result.listings[0].formattedAddress;
+  let price = result.listings[0].price;
+  let bedrooms = result.listings[0].bedrooms;
+  let bathrooms = result.listings[0].bathrooms;
+  let propType = result.listings[0].propertyType;
+  let sqrfootage = result.listings[0].squareFootage;
   //targets in the div to be filled with the data
   $("#listRent").html("$" + listRent);
   $("#rentRangeLow").html("$" + rentRangeLow);
